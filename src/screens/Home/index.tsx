e5c26f1432c6c1e995be840a6f752205f14117c7
Lari Santoni <lari.santoni@gmail.com>
2022-12-24 10:38:20 -0300

    Finalizado modulo 2 do curso

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,71 @@
+import { Text, View, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
+import { styles } from './styles';
+
+import { Participant } from '../../components/Participant';
+
+export function Home(){
+  const participants = ['Tiffany', 'Jiu', 'Goeun', 'Jinsoul', 'Tayeon', 'Yoohyeon', 'Yuki', 'Lip', 'Yuri', 'Seohyun'];
+
+  function handleParticipantAdd(){
+    if(participants.includes('Goeun')){
+      return Alert.alert('Participante existe', 'Já existe um participante na lista com este nome.')
+    }
+  }
+
+  function handleParticipantRemove(name: string){
+    Alert.alert('Remover', `Remover o participante ${name}?`, [
+      {
+        text: 'Sim',
+        onPress: () => Alert.alert('Deletado!')
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ])
+  }
+
+  return(
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Segunda, 19 de Dezembro de 2022.
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input} 
+          placeholder='Nome do participante'
+          placeholderTextColor={'#6B6B6B'}
+          />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={participants}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <Participant 
+            key={item}
+            name={item} 
+            onRemove={() => handleParticipantRemove(item)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no envento ainda? Adicione participantes a sua lista de presença.
+          </Text>
+        )}
+      />
+    </View>
+  )
+}
